dc5408db85b570c128aa36f5cf00f93c72fb3537
tvsnarayana <venkatthatvarthi@gmail.com>
2019-03-23 18:03:11 +0530

    Create wsdeploy.sh

---
+++ wsdeploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#binDir=`dirname ${0}`
+#. ${binDir}/setupCmdLine.sh
+#${WAS_HOME}/bin/wsdeploy.sh "$@"
+./ws_ant.sh -f ./wsdeploy.xml deploy
+
+
+https://www-945.ibm.com/support/fixcentral/swg/selectFixes?parent=ibm~Rational&product=ibm/Rational/IBM+Installation+Manager&function=all
+
+./installc -acceptLicense -log /home/ubuntu/log/sample.log
+web/ibmim -secure
+
+http://www.ibm.com/software/repositorymanager/com.ibm.websphere.ND.v85
